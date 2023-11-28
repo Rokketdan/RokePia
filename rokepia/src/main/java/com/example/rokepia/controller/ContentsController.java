4f972dff9ea9d6bce258b009c0b14a17a6f996dd
Cho Hyeonbi <127538099+bivi99@users.noreply.github.com>
2023-11-28 10:30:03 +0900

    anime template connect

---
+++ rokepia/src/main/java/com/example/rokepia/controller/ContentsController.java
@@ -0,0 +1,16 @@
+package com.example.rokepia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ContentsController {
+	
+	@GetMapping("contents")
+	public String list() {
+		return "contents/categories";
+	}
+}
