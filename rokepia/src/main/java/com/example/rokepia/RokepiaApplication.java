ececa7d36dcbf3c7e3b872eba652ceca95ab6372
TaeHyeong16 <dlxodlxo12@naver.com>
2023-11-28 09:27:29 +0900

    다시시작

--- rokepia/src/main/java/com/example/rokepia/RokepiaApplication.java
+++ rokepia/src/main/java/com/example/rokepia/RokepiaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RokepiaApplication {
-//////
+
 	public static void main(String[] args) {
 		SpringApplication.run(RokepiaApplication.class, args);
 	}
