9704e22dbba0292bb4dbafaca9ad12d88d9ce8f9
TaeHyeong16 <dlxodlxo12@naver.com>
2023-11-28 11:37:04 +0900

    html 수정

--- rokepia/src/main/java/com/example/rokepia/controller/ContentsController.java
+++ rokepia/src/main/java/com/example/rokepia/controller/ContentsController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ContentsController {
 	
 	@GetMapping("contents")
-	public String list() {
-		return "contents/categories";
+	public String anime() {
+		return "contents/anime";
 	}
 }
