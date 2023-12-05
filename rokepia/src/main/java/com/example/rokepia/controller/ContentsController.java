1cdbcecd3ac322d50b96973de14589868d8a3713
Cho Hyeonbi <127538099+bivi99@users.noreply.github.com>
2023-12-05 11:43:58 +0900

    중간수정

--- rokepia/src/main/java/com/example/rokepia/controller/ContentsController.java
+++ rokepia/src/main/java/com/example/rokepia/controller/ContentsController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("contents")
+@Slf4j
 public class ContentsController {
 	
 	@GetMapping("anime")
---
+++ rokepia/src/main/java/com/example/rokepia/model/category/Category.java
@@ -0,0 +1,12 @@
+package com.example.rokepia.model.category;
+
+import lombok.Data;
+
+@Data
+public class Category {
+
+	private Long id;
+	private String name;
+	private String image;
+	 
+}
