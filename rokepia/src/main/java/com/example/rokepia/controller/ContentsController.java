37dbf47d3780089accbb477152a8fb9184b9984a
Cho Hyeonbi <127538099+bivi99@users.noreply.github.com>
2023-12-07 11:04:37 +0900

    재환님 커밋

--- rokepia/src/main/java/com/example/rokepia/controller/ContentsController.java
+++ rokepia/src/main/java/com/example/rokepia/controller/ContentsController.java
@@ -1,14 +1,10 @@
 package com.example.rokepia.controller;
 
 
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.rokepia.model.Location;
-import com.example.rokepia.repository.LocationMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,23 +13,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("contents")
 public class ContentsController {
 
-	private final LocationMapper test;
-	
 	//카테고리 테스트
-	@GetMapping("/")
-  public String home(Model model) {
-		log.info("index 페이지 출력시작");
-		
-		List<Location> humu = test.testAllSelect();
-		model.addAttribute("testModel",humu);
-		log.info("index 페이지 정성작동 함수종료");
-	  return "categories";
+	@GetMapping("anime")
+	public String contents() {
+	  return "contents/anime";
   }
-	@GetMapping("/")
-	  public String test() {
-			
+	@GetMapping("contents_detail")
+	public String contents_detail() {
 			log.info("index 페이지 출력");
 		  return "anime-details";
 	  }
