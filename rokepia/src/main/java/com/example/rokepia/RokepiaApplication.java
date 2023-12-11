77c19b7c49285687f3f37c48c76acb5e4cdf2992
Hwang-gunwoong <95115285+Hwang-gunwoong@users.noreply.github.com>
2023-12-11 09:37:19 +0900

    재설치

---
+++ rokepia/src/main/java/com/example/rokepia/RokepiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rokepia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokepiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokepiaApplication.class, args);
+	}
+
+}
---
+++ rokepia/src/test/java/com/example/rokepia/RokepiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rokepia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RokepiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
